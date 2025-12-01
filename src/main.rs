a2ad6998fd3c4816efec64a05ce4bf53dd6711cf
Grégoire DEFOY <gregoire.defoy@nevermind-e.com>
2025-12-01 09:57:38 +0100

    part 2 brute force

--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,58 @@
-
 fn main() {
-
-    let sol_1 = include_str!("1.in").trim().lines().fold((0, 50), |(mut zero , mut position), order|{
-        let amount = order[1..].parse::<i32>().unwrap();
-        match order.as_bytes()[0] {
-            b'L' => {
-                position -= amount;
-                if position < 0 {
-                    position += 100;
+    let input = include_str!("1.in");
+    let sol_1 = input
+        .trim()
+        .lines()
+        .fold((0, 50), |(mut zero, mut position), order| {
+            let amount = order[1..].parse::<i32>().unwrap();
+            match order.as_bytes()[0] {
+                b'L' => {
+                    position -= amount;
+                    if position < 0 {
+                        position += 100;
+                    }
                 }
+                b'R' => {
+                    position += amount;
+                }
+                _ => panic!(),
             }
-            b'R' =>{
-                position += amount;
+            position %= 100;
+            if position == 0 {
+                zero += 1;
             }
-            _=> panic!()
-        }
-        position %=100;
-        if position == 0 {
-            zero +=1;
-        }
-        (zero ,position)
-
-    });
+            (zero, position)
+        });
     dbg!(sol_1);
 
-    let sol_2 = include_str!("1.in").trim().lines().fold((0, 50), |(mut zero , mut position), order|{
-        let amount = order[1..].parse::<i32>().unwrap();
-        match order.as_bytes()[0] {
-            b'L' => {
-                position -= amount;
-
-                if position < 0 {
-                    zero += (amount/100).max(1);
+    let sol_2 = input
+        .trim()
+        .lines()
+        .fold((0, 50), |(mut zero, mut position), order| {
+            let mut amount = order[1..].parse::<i32>().unwrap();
+            match order.as_bytes()[0] {
+                b'L' => {
+                    while amount != 0 {
+                        position -= 1;
+                        amount -= 1;
+                        if position % 100 == 0 {
+                            zero += 1;
+                        }
+                    }
                 }
-
-            }
-            b'R' =>{
-                position += amount;
-                if position > 99 {
-                    zero += (amount/100).max(1);
+                b'R' => {
+                    while amount != 0 {
+                        position += 1;
+                        amount -= 1;
+                        if position % 100 == 0 {
+                            zero += 1;
+                        }
+                    }
                 }
-
+                _ => panic!(),
             }
-            _=> panic!()
-        }
-        position %=100;
-        if position == 0 {
-            zero +=1;
-        }
-        (zero ,position)
 
-    });
+            (zero, position)
+        });
     dbg!(sol_2);
 }
