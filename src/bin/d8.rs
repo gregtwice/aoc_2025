6110143ddaa0cafafe6b7f047940030890f6fcf1
Grégoire DEFOY <gregoire.defoy@nevermind-e.com>
2025-12-08 14:29:46 +0100

    d8

--- src/bin/d8.rs
+++ src/bin/d8.rs
@@ -15,10 +15,9 @@ fn main() {
                 i.next().unwrap().unwrap(),
             )
         })
-        .collect::<HashSet<(i32, i32, i32)>>();
+        .collect::<HashSet<(i64, i64, i64)>>();
 
-    let mut distances =
-        BTreeMap::<ordered_float::OrderedFloat<f64>, (&(i32, i32, i32), &(i32, i32, i32))>::new();
+    let mut distances = BTreeMap::<i64, (&(i64, i64, i64), &(i64, i64, i64))>::new();
 
     for a in &boxes {
         for b in &boxes {
@@ -27,21 +26,16 @@ fn main() {
                     + (a.1 - b.1) * (a.1 - b.1)
                     + (a.2 - b.2) * (a.2 - b.2);
 
-                let distance = f64::sqrt(f64::from(square));
-
-                distances.insert(distance.into(), (a, b));
+                distances.insert(square, (a, b));
             }
         }
     }
 
-
-    let mut circuits: Vec<HashSet<&(i32, i32, i32)>> = Vec::new();
+    let mut circuits: Vec<HashSet<&(i64, i64, i64)>> = Vec::new();
     let mut joined = HashSet::new();
     distances.iter().take(1000).for_each(|(_d, &(a, b))| {
         let mut ca = None;
         let mut cb = None;
-        // println!("{} {:?} {:?}", _d, a, b);
-
         for (i, circuit) in circuits.iter().enumerate() {
             if circuit.contains(a) {
                 if ca.is_some() {
@@ -82,18 +76,77 @@ fn main() {
 
         joined.insert(*a);
         joined.insert(*b);
-        //
-        // println!("{} {:?} {:?}", _d, a, b);
-        // println!("{:?}", circuits);
     });
 
-
-    let mut cc = circuits.iter().map( | c|  c.len()).collect::<Vec<_>>();
+    let mut cc = circuits.iter().map(|c| c.len()).collect::<Vec<_>>();
     cc.sort_unstable();
 
     dbg!(&cc.iter().rev().take(5).collect::<Vec<_>>());
 
-        let count = cc.iter().rev().take(3).product::<usize>();
-    println!("{}", count );
+    let count = cc.iter().rev().take(3).product::<usize>();
+    println!("{}", count);
 
+    let mut circuits: Vec<HashSet<&(i64, i64, i64)>> = Vec::new();
+
+    let limit = 1000;
+
+    while let Some((_, (a, b))) = distances.pop_first() {
+        let mut ca = None;
+        let mut cb = None;
+        for (i, circuit) in circuits.iter().enumerate() {
+            if circuit.contains(a) {
+                ca = Some(i);
+            }
+            if circuit.contains(b) {
+                cb = Some(i);
+            }
+        }
+
+        match (ca, cb) {
+            (Some(ia), Some(ib)) => {
+                if ia != ib {
+                    // merge circuits
+
+                    let [ca, cb] = circuits.get_disjoint_mut([ia, ib]).unwrap();
+                    ca.extend(cb.iter());
+
+                    if ca.len() >= limit {
+                        dbg!(&a, &b);
+                        dbg!(a.0*b.0);
+                        return;
+                    }
+                    circuits.remove(ib);
+                }
+            }
+            (Some(ia), None) => {
+                circuits[ia].insert(b);
+                if circuits[ia].len() >= limit {
+                    dbg!(&a, &b);
+                    dbg!(a.0 * b.0);
+                    return;
+                }
+            }
+            (None, Some(ib)) => {
+                circuits[ib].insert(a);
+                if circuits[ib].len() >= limit {
+                    dbg!(&a, &b);
+                    dbg!(a.0 * b.0);
+
+                    return;
+                }
+            }
+
+            _ => {
+                circuits.push(HashSet::from([a, b]));
+            }
+        }
+    }
+
+    let mut cc = circuits.iter().map(|c| c.len()).collect::<Vec<_>>();
+    cc.sort_unstable();
+
+    dbg!(&cc.iter().rev().take(5).collect::<Vec<_>>());
+
+    let count = cc.iter().rev().take(3).product::<usize>();
+    println!("{}", count);
 }
